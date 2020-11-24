17d315b73869923b8f86433a658e0288ab672d24
OLUWASEGUN ISRAEL <segunisrael4@gmail.com>
2020-11-24 13:00:01 +0100

    init

--- client/js/reserved.js
+++ client/js/reserved.js
@@ -1,5 +1,5 @@
 var year = new Date().getFullYear();
 
-var date = `&copy; Ariamson ${year}. All Rights Reserved.`;
+var date = `&copy; Ariamson Group ${year}. All Rights Reserved.`;
 
 document.getElementsByTagName('footer')[0].innerHTML = date;
